f147f9d9bcc0aafea0b6e799ed56dc0a450f3080
Pipe1098 <pipe1e@hotmail.com>
2023-07-29 11:30:24 -0500

    proband cambiar nombre endpoint

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,7 +3,13 @@ import { Application, Request, Response } from "express";
 import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
-	app.get("/api", (req: Request, res: Response) => {
+	app.get("/product", (req: Request, res: Response) => {
+		return res.status(200).send(CoursesData);
+	});
+};
+
+export const loadApiEndpoint = (app: Application): void => {
+	app.get("/produ", (req: Request, res: Response) => {
 		return res.status(200).send(CoursesData);
 	});
 };
